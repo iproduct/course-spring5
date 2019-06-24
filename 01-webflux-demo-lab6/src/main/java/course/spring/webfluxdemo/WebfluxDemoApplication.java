fe075ca5bf615e2d18cbedaefbfdd245379278ad
Trayan Iliev <office@iproduct.org>
2019-06-24 12:41:09 +0300

    webflux demo initial commit

---
+++ 01-webflux-demo-lab6/src/main/java/course/spring/webfluxdemo/WebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package course.spring.webfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxDemoApplication.class, args);
+	}
+
+}
