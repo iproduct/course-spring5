8f23ecfdae2ea356f8e7d76ff4714fb5db033539
Trayan Iliev <office@iproduct.org>
2019-06-25 17:30:12 +0300

    static resources added

---
+++ 01-webflux-demo-lab6/src/main/java/course/spring/webfluxdemo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package course.spring.webfluxdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/public", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
+    }
+
+}
