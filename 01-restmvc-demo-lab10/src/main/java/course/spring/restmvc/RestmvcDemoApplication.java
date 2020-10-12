ab0cd9378bd370f357248692685bad0704dbf661
Trayan Iliev <office@iproduct.org>
2020-10-12 11:03:15 +0300

    spring rest mvc lab initial commit

---
+++ 01-restmvc-demo-lab10/src/main/java/course/spring/restmvc/RestmvcDemoApplication.java
@@ -0,0 +1,13 @@
+package course.spring.restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestmvcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestmvcDemoApplication.class, args);
+	}
+
+}
