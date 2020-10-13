826aa89371baeaf9830b0e385f50d7cd5bc3a362
Trayan Iliev <office@iproduct.org>
2020-10-13 19:41:32 +0300

    01-spring-intro-lab11 HelloMVC commit

--- 01-spring-intro-lab11/src/main/java/course/spring/intro/web/HelloMVC.java
+++ 01-spring-intro-lab11/src/main/java/course/spring/intro/web/HelloMVC.java
@@ -1,13 +1,16 @@
 package course.spring.intro.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloMVC {
 
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello from Spring 5!";
+    public String sayHello(
+            @RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+        return String.format("Hello %s, from Spring 5!", name);
     }
+
 }
