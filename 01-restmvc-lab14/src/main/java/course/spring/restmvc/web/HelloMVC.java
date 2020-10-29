568754167bbf7e356eb4feb5ed98c35870b08ced
Trayan Iliev <office@iproduct.org>
2020-10-29 14:30:25 +0200

    01-restmvc-lab14 HelloMVC with path, query and matrix params

---
+++ 01-restmvc-lab14/src/main/java/course/spring/restmvc/web/HelloMVC.java
@@ -0,0 +1,21 @@
+package course.spring.restmvc.web;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloMVC {
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(name = "name", required = false, defaultValue = "Stranger") String name) {
+        return String.format("Hello %s, from Spring Web MVC!", name);
+    }
+
+    @GetMapping({"/users/{userId}/posts/{postId}", "/users/{userId}/posts"})
+    public String getUserPost(@PathVariable(name = "userId") Integer userId,
+                              @MatrixVariable(name="mode", pathVar="userId") String userMode,
+                              @PathVariable(name = "postId", required = false) Integer postId,
+                              @MatrixVariable(name="mode", pathVar="postId") String postMode) {
+        return String.format("User: %d in mode: %s -> Post: %d in mode: %s%n", userId, userMode, postId, postMode);
+    }
+
+}
