1dfa87a4860ec173c0e8c20108adc5aa66326466
Trayan Iliev <office@iproduct.org>
2020-10-30 12:24:53 +0200

    headers demo added

--- 01-restmvc-lab14/src/main/java/course/spring/restmvc/web/HelloMVC.java
+++ 01-restmvc-lab14/src/main/java/course/spring/restmvc/web/HelloMVC.java
@@ -1,6 +1,12 @@
 package course.spring.restmvc.web;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v2")
@@ -17,6 +23,24 @@ public class HelloMVC {
                               @PathVariable(name = "postId", required = false) Integer postId,
                               @MatrixVariable(name="mode", pathVar="postId") String postMode) {
         return String.format("User: %d in mode: %s -> Post: %d in mode: %s%n", userId, userMode, postId, postMode);
+    }
+
+    @GetMapping("/hello-headers")
+    public String getRequestHeaders(@RequestHeader("Accept") String accept){
+        return String.format("Access: %s", accept);
+    }
+
+    @GetMapping(path="/hello-all-headers", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, String> getRequestHeaders(HttpServletRequest request){
+        Map<String, String> headers = new LinkedHashMap<>();
+        request.getHeaderNames().asIterator()
+                .forEachRemaining(headerName -> {
+                    StringBuilder values = new StringBuilder();
+                    request.getHeaders(headerName).asIterator()
+                            .forEachRemaining(val -> values.append(val).append(", "));
+                    headers.put(headerName, values.toString());
+                });
+        return headers;
     }
 
 
