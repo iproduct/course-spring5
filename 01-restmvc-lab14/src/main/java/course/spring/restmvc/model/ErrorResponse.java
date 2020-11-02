611ba5ab891ab7378d5de7ef0900dd72fb832498
Trayan Iliev <office@iproduct.org>
2020-11-02 12:09:51 +0200

    CRUD, with error handling

---
+++ 01-restmvc-lab14/src/main/java/course/spring/restmvc/model/ErrorResponse.java
@@ -0,0 +1,26 @@
+package course.spring.restmvc.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    @NonNull
+    int status;
+    @NonNull
+    String message;
+    List<String> violations = new ArrayList<>();
+    LocalDateTime timestamp = LocalDateTime.now();
+
+    public ErrorResponse(@NonNull int status, @NonNull String message, List<String> violations) {
+        this.status = status;
+        this.message = message;
+        this.violations = violations;
+    }
+}
--- 01-restmvc-lab14/src/main/java/course/spring/restmvc/web/PostResource.java
+++ 01-restmvc-lab14/src/main/java/course/spring/restmvc/web/PostResource.java
@@ -2,8 +2,12 @@ package course.spring.restmvc.web;
 
 import course.spring.restmvc.dao.PostRepository;
 import course.spring.restmvc.entity.Post;
+import course.spring.restmvc.exception.EntityNotFoundException;
+import course.spring.restmvc.exception.InvalidEntityDataException;
+import course.spring.restmvc.model.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -22,7 +26,9 @@ public class PostResource {
 
     @GetMapping("/{id}")
     public Post getPostById(@PathVariable("id") Long id) {
-        return postRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+//        return postRepo.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+//                String.format("Post with ID:%s not found.", id)));
+        return postRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(
                 String.format("Post with ID:%s not found.", id)));
     }
 
@@ -34,6 +40,12 @@ public class PostResource {
 
     @PutMapping("/{id}")
     public Post updatePost(@PathVariable("id") Long id, @RequestBody Post post) {
+        Post found = postRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(
+                String.format("Post with ID:%s not found.", id)));
+        if(!found.getId().equals(post.getId())) {
+           throw new InvalidEntityDataException(
+                    String.format("Post with ID:%d is different from resource URL ID: %d.", post.getId(), id));
+        }
         return postRepo.save(post);
     }
 
@@ -42,6 +54,13 @@ public class PostResource {
          Post deleted = postRepo.findById(id).orElseThrow();
          postRepo.deleteById(id);
          return deleted;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage())
+        );
     }
 
 }
