a94d03b40df93fa74c6c7c1bcb66286e3f1b12b5
Trayan Iliev <office@iproduct.org>
2020-11-18 21:12:04 +0200

    User class modified, sample users added

--- 01-restmvc-lab12/src/main/java/course/spring/restmvc/init/DataInitializer.java
+++ 01-restmvc-lab12/src/main/java/course/spring/restmvc/init/DataInitializer.java
@@ -1,6 +1,7 @@
 package course.spring.restmvc.init;
 
 import course.spring.restmvc.model.Post;
+import course.spring.restmvc.model.User;
 import course.spring.restmvc.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,22 +10,24 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Set;
 
+import static course.spring.restmvc.model.Role.*;
+
 @Component
 public class DataInitializer implements CommandLineRunner {
     @Autowired
     private PostService postService;
 
-//    private static final List<User> SAMPLE_USERS = List.of(
-//            new User("Default", "Admin", "admin", "admin",
-//                    "https://lh3.googleusercontent.com/proxy/cGNVbclL0E2LBuUnIxUaC7d-wP_K18xwNUMVzCHmtxgdEtaknGpKCZz-rBVUWP46jCXJSPq6Va7hZ__JZVjG4EKwLx-Kezlk9Qtb5NDc9Gb-E1oq85KV",
-//                    Set.of(READER, AUTHOR, ADMIN)),
-//            new User("Default", "Author", "author", "author",
-//                    "https://cdn.iconscout.com/icon/premium/png-512-thumb/public-domain-user-618551.png",
-//                    Set.of(AUTHOR)),
-//            new User("Default", "Reader", "reader", "reader",
-//                    "https://www.publicdomainpictures.net/pictures/230000/velka/computer-user.jpg",
-//                    Set.of(READER))
-//    );
+    private static final List<User> SAMPLE_USERS = List.of(
+            new User("Default", "Admin", "admin", "admin",
+                    "https://lh3.googleusercontent.com/proxy/cGNVbclL0E2LBuUnIxUaC7d-wP_K18xwNUMVzCHmtxgdEtaknGpKCZz-rBVUWP46jCXJSPq6Va7hZ__JZVjG4EKwLx-Kezlk9Qtb5NDc9Gb-E1oq85KV",
+                    Set.of(READER, AUTHOR, ADMIN)),
+            new User("Default", "Author", "author", "author",
+                    "https://cdn.iconscout.com/icon/premium/png-512-thumb/public-domain-user-618551.png",
+                    Set.of(AUTHOR)),
+            new User("Default", "Reader", "reader", "reader",
+                    "https://www.publicdomainpictures.net/pictures/230000/velka/computer-user.jpg",
+                    Set.of(READER))
+    );
     private static final List<Post> SAMPLE_POSTS = List.of(
             new Post("New in Spring 5", "WebFlux is here ...",
                     "https://www.publicdomainpictures.net/pictures/40000/velka/spring-flowers-13635086725Z1.jpg",
--- 01-restmvc-lab12/src/main/java/course/spring/restmvc/model/User.java
+++ 01-restmvc-lab12/src/main/java/course/spring/restmvc/model/User.java
@@ -41,14 +41,28 @@ public class User {
     @NonNull
     @Size(min=6)
     private String password;
-
     @NonNull
     @URL
     private String imageUrl;
-    private Set<Role> keywords = Set.of();
+    private boolean active = true;
+    private Set<Role> roles = Set.of(Role.READER, Role.AUTHOR);
     @PastOrPresent
     private LocalDateTime created = LocalDateTime.now();
     @PastOrPresent
     private LocalDateTime modified = LocalDateTime.now();
+
+
+    public User(@NonNull @NotNull @Size(min = 2, max = 30) String firstName,
+                @NonNull @NotNull @Size(min = 2, max = 30) String lastName,
+                @NonNull @NotNull @Size(min = 2, max = 30) String username,
+                @NonNull @Size(min = 6) String password,
+                @NonNull @URL String imageUrl, Set<Role> roles) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.imageUrl = imageUrl;
+        this.roles = roles;
+    }
 
 }
