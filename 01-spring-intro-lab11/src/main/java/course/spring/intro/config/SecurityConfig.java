31f893335ffbbdb1868b8c82787b90438690fb30
Trayan Iliev <office@iproduct.org>
2020-11-24 19:34:54 +0200

    JwtAuthenticationEntryPoint added

--- 01-spring-intro-lab11/src/main/java/course/spring/intro/config/SecurityConfig.java
+++ 01-spring-intro-lab11/src/main/java/course/spring/intro/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package course.spring.intro.config;
 
 import course.spring.intro.service.UserService;
+import course.spring.intro.web.JwtAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import static course.spring.intro.model.Role.ADMIN;
@@ -13,6 +16,10 @@ import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -23,8 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(DELETE, "/api/posts").hasAnyRole(AUTHOR.toString(), ADMIN.toString())
             .antMatchers("/api/users/**").hasRole(ADMIN.toString())
             .antMatchers("/**").permitAll()
-                .and()
-                    .httpBasic();
+            .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
