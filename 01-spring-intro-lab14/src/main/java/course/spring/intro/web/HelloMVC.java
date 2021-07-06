8e237375659979f9040e73ef4aaf70c3bb671129
Trayan Iliev <office@iproduct.org>
2021-07-06 14:46:14 +0300

    handler method arguments by DI

--- 01-spring-intro-lab14/src/main/java/course/spring/intro/web/HelloMVC.java
+++ 01-spring-intro-lab14/src/main/java/course/spring/intro/web/HelloMVC.java
@@ -1,6 +1,11 @@
 package course.spring.intro.web;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
@@ -18,20 +23,28 @@ public class HelloMVC {
     public String welcomeUser(
             @RequestParam(value = "username", required = false) String username,
             @RequestHeader("Accept") String accept,
-            @CookieValue(value = "JSESSIONID", required = false) String sessionId
+            @CookieValue(value = "JSESSIONID", required = false) String sessionId,
+            HttpSession session,
+            HttpServletRequest request
     ) {
         StringBuilder sb = new StringBuilder(
                 String.format("<h2>Welcome, %s!</h2>", username == null ? "User" : username));
 
         // Get headers
         sb.append("<h3>Request Headers:</h3><ul>");
-        sb.append("<li>Accept: ").append(accept).append("</li>");
+        sb.append("<li>Accept: ").append(accept).append("</li><hr>");
+        request.getHeaderNames().asIterator().forEachRemaining(hname ->{
+            sb.append("<li>").append(hname).append(": ").append(request.getHeader(hname)).append("</li>");
+        });
         sb.append("</ul>");
 
         // Get cookies
-        sb.append("<br><h3>Request cookies:</h3><ul>");
-        sb.append("<li>JSESSIONID: ").append(sessionId).append("</li>");
+        sb.append("<hr><h3>Request cookies:</h3><ul>");
+        for(Cookie c: request.getCookies()) {
+            sb.append("<li>").append(c.getName()).append(": ").append(c.getValue()).append("</li>");
+        }
         sb.append("</ul>");
+        session.invalidate();
         return sb.toString();
     }
 }
