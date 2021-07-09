2fbe0d822aa6416bffbd0f341a0304532a6e7250
Trayan Iliev <office@iproduct.org>
2021-07-09 11:40:51 +0300

    springboot starter jdbc added + PostRepositoryJdbc implementation

---
+++ 02-blogs-api-lab14/src/main/java/course/spring/blogs/dao/impl/PostRepositoryJdbc.java
@@ -0,0 +1,43 @@
+package course.spring.blogs.dao.impl;
+
+import course.spring.blogs.dao.PostRepository;
+import course.spring.blogs.entity.Post;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class PostRepositoryJdbc implements PostRepository {
+    private JdbcTemplate jdbcTemplate;
+
+
+    @Override
+    public Collection<Post> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Post> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Post create(Post post) {
+        return null;
+    }
+
+    @Override
+    public Post update(Post post) {
+        return null;
+    }
+
+    @Override
+    public Post deleteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+}
