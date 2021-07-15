6c108aad2327b34041a073f42fafb6de69e08e54
Trayan Iliev <office@iproduct.org>
2021-07-15 12:41:01 +0300

    username argument added to findByUsername method

--- 06-blogs-api-data-jpa-lab14/src/main/java/course/spring/blogs/dao/UserRepository.java
+++ 06-blogs-api-data-jpa-lab14/src/main/java/course/spring/blogs/dao/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername();
+    Optional<User> findByUsername(String username);
 }
