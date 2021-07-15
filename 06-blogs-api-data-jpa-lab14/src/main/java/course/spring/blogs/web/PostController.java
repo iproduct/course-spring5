8e8147063e7df24cc8ea3dbf7f70a915038d1030
Trayan Iliev <office@iproduct.org>
2021-07-15 13:02:25 +0300

    Web Controller for Posts completed

--- 06-blogs-api-data-jpa-lab14/src/main/java/course/spring/blogs/web/PostController.java
+++ 06-blogs-api-data-jpa-lab14/src/main/java/course/spring/blogs/web/PostController.java
@@ -1,6 +1,7 @@
 package course.spring.blogs.web;
 
 import course.spring.blogs.entity.Post;
+import course.spring.blogs.exception.InvalidEntityDataException;
 import course.spring.blogs.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,12 +21,32 @@ public class PostController {
         return postService.getAllPosts();
     }
 
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable Long id) {
+        return postService.getPostById(id);
+    }
+
     @PostMapping
     public ResponseEntity<Post> addPost(@RequestBody Post post) {
         Post created = postService.addPost(post);
         return ResponseEntity.created(
-            ServletUriComponentsBuilder.fromCurrentRequest().
-                    pathSegment("{id}").buildAndExpand(created.getId()).toUri())
-            .body(created);
+                ServletUriComponentsBuilder.fromCurrentRequest().
+                        pathSegment("{id}").buildAndExpand(created.getId()).toUri())
+                .body(created);
     }
+
+    @PutMapping("/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+        if(! id.equals(post.getId())) {
+            throw new InvalidEntityDataException(String.format("Post ID=%d is different from URL ID=%d",
+                    post.getId(), id));
+        }
+        return postService.updatePost(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public Post deletePost(@PathVariable Long id) {
+        return postService.deletePostById(id);
+    }
+
 }
