1adf84ea8fc984b498f3628a1dcdc70ffe764b70
Trayan Iliev <office@iproduct.org>
2021-07-16 10:03:57 +0300

    Constarint violations -> 400 BAD_REQUEST

--- 06-blogs-api-data-jpa-lab14/src/main/java/course/spring/blogs/web/ErrorHandlerControllerAdvice.java
+++ 06-blogs-api-data-jpa-lab14/src/main/java/course/spring/blogs/web/ErrorHandlerControllerAdvice.java
@@ -4,9 +4,13 @@ import course.spring.blogs.dto.ErrorResponse;
 import course.spring.blogs.exception.InvalidEntityDataException;
 import course.spring.blogs.exception.NonexistingEntityException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -31,6 +35,16 @@ public class ErrorHandlerControllerAdvice {
 
     @ExceptionHandler()
     public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException e) {
-        ResponseEntity.badRequest().body(new ErrorResponse())
+        return ResponseEntity.badRequest().body(new ErrorResponse(BAD_REQUEST.value(), e.getMessage(),
+                e.getBindingResult().getAllErrors().stream().map(err -> {
+                            if (err instanceof FieldError) {
+                                FieldError ferr = (FieldError) err;
+                                return String.format("'%s=%s': %s",
+                                        ferr.getField(), ferr.getRejectedValue(), ferr.getDefaultMessage());
+                            } else {
+                                return err.getDefaultMessage();
+                            }
+                        }
+                ).collect(Collectors.toList())));
     }
 }
