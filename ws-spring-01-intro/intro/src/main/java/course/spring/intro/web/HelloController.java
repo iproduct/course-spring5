156dd28cb9e597dc72f0bddad63ed31075c5c565
Trayan Iliev <office@iproduct.org>
2022-02-01 17:59:31 +0200

    first service endpoint added

---
+++ ws-spring-01-intro/intro/src/main/java/course/spring/intro/web/HelloController.java
@@ -0,0 +1,15 @@
+package course.spring.intro.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello from Spring MVC!";
+    }
+}
