405ad8e9ead67cd85d550362b7c553c6a9ae8c08
Trayan Iliev <office@iproduct.org>
2022-02-02 18:15:55 +0200

    Article model added + REST endpoint

--- ws-spring-01-intro/intro/src/main/java/course/spring/intro/entity/Article.java
+++ ws-spring-01-intro/intro/src/main/java/course/spring/intro/entity/Article.java
@@ -2,14 +2,19 @@ package course.spring.intro.entity;
 
 import lombok.*;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.Set;
 
+@Entity
 @Data
 @NoArgsConstructor
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class Article {
+    @Id
     @NonNull
     private Long id;
     @NonNull
@@ -18,6 +23,7 @@ public class Article {
     private String content;
     @NonNull
     private String auhor;
+    @ElementCollection
     @NonNull
     private Set<String> keywords = Set.of();
     private LocalDateTime created = LocalDateTime.now();
