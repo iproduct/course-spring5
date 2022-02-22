0d5bc3f55787cc83e3bb72630db1617823221873
Trayan Iliev <office@iproduct.org>
2022-02-22 12:41:29 +0200

    xml beans configuration demo

--- ws2-spring-03-core-xml/src/main/java/course/ws/AnnotationContextDemoMain.java
+++ ws2-spring-03-core-xml/src/main/java/course/ws/AnnotationContextDemoMain.java
@@ -4,14 +4,15 @@ import course.ws.client.ArticlePresenter;
 import course.ws.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationContextDemoMain {
     public static void main(String[] args) {
-        ApplicationContext ctx = new AnnotationConfigApplicationContext("course.ws");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
         ArticlePresenter presenter = ctx.getBean(ArticlePresenter.class);
         presenter.present();
-        System.out.println();
-        UserService userService = ctx.getBean(UserService.class);
-        userService.getAllUsers().forEach(System.out::println);
+//        System.out.println();
+//        UserService userService = ctx.getBean(UserService.class);
+//        userService.getAllUsers().forEach(System.out::println);
     }
 }
