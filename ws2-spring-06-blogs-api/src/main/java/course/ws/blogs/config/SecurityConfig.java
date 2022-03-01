c08eddb2ca7cf3caff4ce11d3f11b27665af165d
Trayan Iliev <office@iproduct.org>
2022-03-01 12:49:15 +0200

    SecurityConfig completed

--- ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/config/SecurityConfig.java
+++ ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/config/SecurityConfig.java
@@ -3,14 +3,19 @@ package course.ws.blogs.config;
 import course.ws.blogs.entity.Role;
 import course.ws.blogs.exception.EntityNotFoundException;
 import course.ws.blogs.service.UserService;
+import course.ws.blogs.web.jwt.JwtAuthenticationEntryPoint;
+import course.ws.blogs.web.jwt.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static course.ws.blogs.entity.Role.ADMIN;
 import static course.ws.blogs.entity.Role.AUTHOR;
@@ -19,6 +24,11 @@ import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -27,7 +37,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(GET, "/api/users","/api/users/**").hasRole(ADMIN.name())
                 .mvcMatchers("/**").hasAnyRole(ADMIN.name(), AUTHOR.name())
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
