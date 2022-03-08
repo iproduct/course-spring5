b4f0d92c0ee1b1539ec91185a4c693aefffc5c5c
Trayan Iliev <office@iproduct.org>
2022-03-08 10:13:05 +0200

    ArticleToArticleDetailDtoMapping added

---
+++ ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/dto/mapping/ArticleToArticleDetailDtoMapping.java
@@ -0,0 +1,23 @@
+package course.ws.blogs.dto.mapping;
+
+import com.github.jmnarloch.spring.boot.modelmapper.PropertyMapConfigurerSupport;
+import course.ws.blogs.dto.ArticleDetailDto;
+import course.ws.blogs.entity.Article;
+import org.modelmapper.PropertyMap;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleToArticleDetailDtoMapping extends PropertyMapConfigurerSupport<Article, ArticleDetailDto> {
+
+    @Override
+    public PropertyMap<Article, ArticleDetailDto> mapping() {
+        return new PropertyMap<Article, ArticleDetailDto>() {
+            @Override
+            protected void configure() {
+                map().setAuthor(
+                        source.getAuthor().getFirstName() + " " +  source.getAuthor().getLastName()
+                );
+            }
+        };
+    }
+}
