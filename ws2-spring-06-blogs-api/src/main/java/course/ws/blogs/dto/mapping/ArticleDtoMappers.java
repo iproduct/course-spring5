659cfe7518d1b5e1c110651f9490ac916425fb2e
Trayan Iliev <office@iproduct.org>
2022-03-08 11:37:25 +0200

    ArticleDtoMappers methods added

---
+++ ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/dto/mapping/ArticleDtoMappers.java
@@ -0,0 +1,28 @@
+package course.ws.blogs.dto.mapping;
+
+import course.ws.blogs.dto.ArticleCreateDto;
+import course.ws.blogs.dto.ArticleDetailDto;
+import course.ws.blogs.dto.ArticleUpdateDto;
+import course.ws.blogs.entity.Article;
+import org.springframework.beans.BeanUtils;
+
+public class ArticleDtoMappers {
+    public static Article mapArticleCreateDtoToArticle(ArticleCreateDto source) {
+        Article result = new Article();
+        BeanUtils.copyProperties(source, result);
+        return result;
+    }
+    public static Article mapArticleUpdateDtoToArticle(ArticleUpdateDto source) {
+        Article result = new Article();
+        BeanUtils.copyProperties(source, result);
+        return result;
+    }
+    public static ArticleDetailDto mapArticleToArticleDetailDto(Article source) {
+        ArticleDetailDto result = new ArticleDetailDto();
+        BeanUtils.copyProperties(source, result);
+        result.setAuthor(
+                source.getAuthor().getFirstName() + " " +  source.getAuthor().getLastName()
+        );
+        return result;
+    }
+}
--- ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/web/ArticleController.java
+++ ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/web/ArticleController.java
@@ -5,14 +5,10 @@ import course.ws.blogs.dto.ArticleDetailDto;
 import course.ws.blogs.entity.Article;
 import course.ws.blogs.entity.User;
 import course.ws.blogs.exception.InvalidEntityDataException;
-import course.ws.blogs.exception.UnautorizedException;
 import course.ws.blogs.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,8 +16,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import static course.ws.blogs.dto.mapping.ArticleDtoMappers.mapArticleCreateDtoToArticle;
+import static course.ws.blogs.dto.mapping.ArticleDtoMappers.mapArticleToArticleDetailDto;
 import static course.ws.blogs.util.ErrorHandlingUtils.checkErrors;
 import static course.ws.blogs.util.UserUtils.getUser;
 
@@ -30,12 +27,10 @@ import static course.ws.blogs.util.UserUtils.getUser;
 @Slf4j
 public class ArticleController {
     private ArticleService articleService;
-    private ModelMapper mapper;
 
     @Autowired
-    public ArticleController(ArticleService articleService, ModelMapper mapper) {
+    public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
-        this.mapper = mapper;
     }
 
     @GetMapping
@@ -53,18 +48,14 @@ public class ArticleController {
     public ResponseEntity<ArticleDetailDto> addNewArticle(@Valid @RequestBody ArticleCreateDto articleDto, Errors errors,
                                                           Principal principal) {
         checkErrors(errors);
-        log.info("Logged User: {}", principal.toString());
-        Article article = mapper.map(articleDto, Article.class);
+        Article article = mapArticleCreateDtoToArticle(articleDto);
         User user = getUser(principal);
         article.setAuthor(user);
-//        Article article1 = new Article();
-//        BeanUtils.copyProperties(articleDto, article1);
-
         Article newArticle = articleService.create(article);
         return ResponseEntity.created(
                         ServletUriComponentsBuilder.fromCurrentRequest().pathSegment("{id}")
                                 .buildAndExpand(newArticle.getId()).toUri())
-                .body(mapper.map(newArticle, ArticleDetailDto.class));
+                .body(mapArticleToArticleDetailDto(newArticle));
     }
 
 
