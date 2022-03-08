1c47e7b5c6cfba41b9be9f866acf009cae9a5093
Trayan Iliev <office@iproduct.org>
2022-03-08 11:02:59 +0200

    getUser(Principal p) utility method added

---
+++ ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/util/UserUtils.java
@@ -0,0 +1,21 @@
+package course.ws.blogs.util;
+
+import course.ws.blogs.entity.User;
+import course.ws.blogs.exception.UnautorizedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.security.Principal;
+
+public class UserUtils {
+    public static User getUser(Principal principal) {
+        if(principal == null || !(principal instanceof UsernamePasswordAuthenticationToken)) {
+            throw new UnautorizedException("Unauthorised. No authenticated user.");
+        }
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) principal;
+        if(!(token.getPrincipal() instanceof User)) {
+            throw new UnautorizedException("Unauthorised. No authenticated user.");
+        }
+        User user = (User)token.getPrincipal();
+        return user;
+    }
+}
--- ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/web/ArticleController.java
+++ ws2-spring-06-blogs-api/src/main/java/course/ws/blogs/web/ArticleController.java
@@ -12,6 +12,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static course.ws.blogs.util.ErrorHandlingUtils.checkErrors;
+import static course.ws.blogs.util.UserUtils.getUser;
 
 @RestController
 @RequestMapping("/api/articles")
@@ -51,12 +53,10 @@ public class ArticleController {
     public ResponseEntity<ArticleDetailDto> addNewArticle(@Valid @RequestBody ArticleCreateDto articleDto, Errors errors,
                                                           Principal principal) {
         checkErrors(errors);
+        log.info("Logged User: {}", principal.toString());
         Article article = mapper.map(articleDto, Article.class);
-        if(principal == null || !(principal instanceof User)) {
-            throw new UnautorizedException("Unauthorised. No authenticated user.");
-        }
-        article.setAuthor((User)principal);
-        log.info("Logged User: {}", principal.getName());
+        User user = getUser(principal);
+        article.setAuthor(user);
 //        Article article1 = new Article();
 //        BeanUtils.copyProperties(articleDto, article1);
 
@@ -66,6 +66,8 @@ public class ArticleController {
                                 .buildAndExpand(newArticle.getId()).toUri())
                 .body(mapper.map(newArticle, ArticleDetailDto.class));
     }
+
+
 
     @PutMapping("/{id:\\d+}")
     public Article updateArticle(@Valid @RequestBody Article article, Errors errors, @PathVariable Long id) {
