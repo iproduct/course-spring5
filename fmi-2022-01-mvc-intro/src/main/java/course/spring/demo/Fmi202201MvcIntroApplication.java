0609f378a857fa7f5cd63b2d88dfa094d0688e9e
Trayan Iliev <office@iproduct.org>
2022-10-05 20:36:04 +0300

    spring mvc intro project inital commit

---
+++ fmi-2022-01-mvc-intro/src/main/java/course/spring/demo/Fmi202201MvcIntroApplication.java
@@ -0,0 +1,13 @@
+package course.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fmi202201MvcIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fmi202201MvcIntroApplication.class, args);
+	}
+
+}
---
+++ fmi-2022-01-mvc-intro/src/test/java/course/spring/demo/Fmi202201MvcIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package course.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fmi202201MvcIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
