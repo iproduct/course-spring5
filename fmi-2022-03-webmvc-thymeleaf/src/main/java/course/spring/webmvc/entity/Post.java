d5ebbc581ca80505911bcfa380f8c9138eb7131b
Trayan Iliev <office@iproduct.org>
2022-11-16 20:53:43 +0200

    Post and DAO Repository added

---
+++ fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/entity/Post.java
@@ -0,0 +1,34 @@
+package course.spring.webmvc.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String title;
+    @NonNull
+    private String content;
+
+    @ElementCollection
+    private Set<String> tags = Set.of();
+
+    public Post(@NonNull String title, @NonNull String content, Set<String> tags) {
+        this.title = title;
+        this.content = content;
+        this.tags = tags;
+    }
+
+    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime modified = LocalDateTime.now();
+}
---
+++ fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/init/DataInitializer.java
@@ -0,0 +1,31 @@
+package course.spring.webmvc.init;
+
+import course.spring.demo.dao.PostRepository;
+import course.spring.demo.entity.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+    public static  final List<Post> SAMPLE_POSTS = List.of(
+            new Post("Novelties in Spring 5", "WebFlux is new reactive web API in Spring 5 ...",
+                    Set.of("spring", "novelty", "webflux")),
+            new Post( "Spring Data is Easy", "Spring Data generates your queris automatically ...",
+                    Set.of("spring data", "spring", "intro")),
+            new Post( "Spring Boot", "Spring Boot allows easy configuration of your project ...")
+    );
+
+    @Autowired
+    private PostRepository postsRepo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(postsRepo.count() == 0) {
+            postsRepo.saveAll(SAMPLE_POSTS);
+        }
+    }
+}
