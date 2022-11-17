c5cde69f8826ec8aef5ff8b7e53a7ef0d10ff9e6
Trayan Iliev <office@iproduct.org>
2022-11-17 11:21:54 +0200

    articles controller + thymeleaf template added

---
+++ fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/web/PostsController.java
@@ -0,0 +1,25 @@
+package course.spring.webmvc.web;
+
+import course.spring.webmvc.service.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+public class PostsController {
+    private PostsService postsService;
+
+    @Autowired
+    public PostsController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
+    @GetMapping
+    public String getArticles(Model model) {
+        model.addAttribute("posts", postsService.getAllPosts());
+        return "articles";
+    }
+}
