86aad1849b5207d3dd7ba4c4801f7eb6d53e784c
Trayan Iliev <office@iproduct.org>
2022-11-23 23:01:17 +0200

    css of menu refactored to bootstrap 5, 'tags' field added to New Post form

--- fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/web/PostsController.java
+++ fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/web/PostsController.java
@@ -2,20 +2,28 @@ package course.spring.webmvc.web;
 
 import course.spring.webmvc.entity.Post;
 import course.spring.webmvc.service.PostsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping("/posts")
+@Slf4j
 public class PostsController {
     private PostsService postsService;
 
@@ -51,4 +59,7 @@ public class PostsController {
         model.addAttribute("title", title);
         return  "post-form";
     }
+
+
+
 }
