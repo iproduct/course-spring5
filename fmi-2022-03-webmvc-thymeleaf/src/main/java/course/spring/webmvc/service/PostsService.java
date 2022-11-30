4330b06d1acc36eb48be867be9ffc7fe98d374a9
Trayan Iliev <office@iproduct.org>
2022-11-30 18:43:41 +0200

    PostService CRUD methods completed

--- fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/service/PostsService.java
+++ fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/service/PostsService.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public interface PostsService {
     List<Post> getAllPosts();
+    Post getPostById(Long id);;
     Post add(Post post);
     Post update(Post post);
+    Post deleteById(Long id);
 }
--- fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/service/impl/PostsServiceImpl.java
+++ fmi-2022-03-webmvc-thymeleaf/src/main/java/course/spring/webmvc/service/impl/PostsServiceImpl.java
@@ -2,10 +2,12 @@ package course.spring.webmvc.service.impl;
 
 import course.spring.webmvc.dao.PostRepository;
 import course.spring.webmvc.entity.Post;
+import course.spring.webmvc.exception.NotFoundException;
 import course.spring.webmvc.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -22,13 +24,33 @@ public class PostsServiceImpl implements PostsService {
     }
 
     @Override
+    public Post getPostById(Long id) {
+        return postRepo.findById(id).orElseThrow(() -> new NotFoundException(
+                String.format("Post with ID='%d' not found", id)
+        ));
+    }
+
+    @Override
     public Post add(Post post) {
+        var now = LocalDateTime.now();
+        post.setCreated(now);
+        post.setModified(now);
         return postRepo.save(post);
     }
 
     @Override
     public Post update(Post post) {
+        var old = getPostById(post.getId());
+        post.setCreated(old.getCreated());
+        post.setModified(LocalDateTime.now());
         return postRepo.save(post);
+    }
+
+    @Override
+    public Post deleteById(Long id) {
+        var deleted = getPostById(id);
+        postRepo.deleteById(id);
+        return deleted;
     }
 
 }
