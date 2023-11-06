6d503fc3c3999fca33fc58f0f8b16dd91ccfb4bb
Trayan Iliev <office@iproduct.org>
2023-11-06 19:02:51 +0200

    spring core project initial commit

---
+++ fmi-2023-02-spring-core-xmlconfig/fmi-2023-02-spring-core-xmlconfig/src/main/java/course/spring/Main.java
@@ -0,0 +1,10 @@
+package course.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("app-context.xml");
+    }
+}
