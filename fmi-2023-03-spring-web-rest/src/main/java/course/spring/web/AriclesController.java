1e6b4dfe1c0ef06426959f3f8993511469ce0edb
Trayan Iliev <office@iproduct.org>
2023-11-13 21:38:35 +0200

    repository added

--- fmi-2023-03-spring-web-rest/src/main/java/course/spring/web/AriclesController.java
+++ fmi-2023-03-spring-web-rest/src/main/java/course/spring/web/AriclesController.java
@@ -1,4 +1,26 @@
 package course.spring.web;
 
+import course.spring.dao.ArticleRepository;
+import course.spring.model.Article;
+import course.spring.provider.ArticleProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/articles")
 public class AriclesController {
+    @Autowired
+    @Qualifier("defaultProvider")
+    private ArticleProvider articleProvider;
+
+
+    @GetMapping
+    List<Article> getAllArticles() {
+        return articleProvider.getArticles();
+    }
 }
