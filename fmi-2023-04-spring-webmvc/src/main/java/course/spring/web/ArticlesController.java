d44847cb532eb79d2a427d7d845ab54428c3981a
Trayan Iliev <office@iproduct.org>
2023-12-04 19:47:35 +0200

    POST new article using form

--- fmi-2023-04-spring-webmvc/src/main/java/course/spring/web/ArticlesController.java
+++ fmi-2023-04-spring-webmvc/src/main/java/course/spring/web/ArticlesController.java
@@ -1,16 +1,25 @@
 package course.spring.web;
 
+import course.spring.model.Article;
 import course.spring.provider.ArticleProvider;
 import course.spring.qualifiers.RepoBacked;
 import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class ArticlesController {
     @Inject
     @Qualifier("repoProvider")
@@ -18,12 +27,56 @@ public class ArticlesController {
     private ArticleProvider articleProvider;
 
     @GetMapping
-    public String getHome(){
+    public String getHome(Model model){
+        model.addAttribute("path", "/");
         return "home";
     }
     @GetMapping("/articles")
     public String getArticles(Model model){
+        model.addAttribute("path", "/articles");
         model.addAttribute("articles", articleProvider.getArticles());
         return "articles";
+    }
+
+    @GetMapping("/articles/add")
+    public String addArticle(@ModelAttribute("article") Article article, Model model){
+        model.addAttribute("path", "/articles/add");
+        return "article-form";
+    }
+
+    @PostMapping("/article-form")
+    public String addArticle(
+            @Valid @ModelAttribute ("article") Article article,
+            BindingResult errors,
+            @RequestParam("file") MultipartFile file,
+            Model model) {
+//            ,@NotNull
+//            Authentication auth) {
+//        User author = (User)auth.getPrincipal();
+//        model.addAttribute("fileError", null);
+//        if(!file.isEmpty() && file.getOriginalFilename().length() > 4) {
+//            if (Pattern.matches(".+\\.(png|jpg|jpeg)", file.getOriginalFilename())) {
+//                handleFile(file, article);
+//            } else {
+//                model.addAttribute("fileError", "Submit PNG or JPG picture please!");
+//                return "article-form";
+//            }
+//        }
+//        if(author == null) {
+//            errors.addError(new ObjectError("article", "No authenticated author"));
+//            return "article-form";
+//        }
+        if(errors.hasErrors()) {
+            return "article-form";
+        }
+        if(article.getId() == null) {  // create
+            log.info("Create new article: {}", article);
+            articleProvider.createArticle(article);
+        }
+//        } else { //edit
+//            log.info("Edit article: {}", article);
+//            articleProvider.(article);
+//        }
+        return "redirect:/articles";
     }
 }
