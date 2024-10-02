35b0d93e60637cd02a543f3b45c155871f12495f
Trayan Iliev <office@iproduct.org>
2024-10-02 22:32:33 +0300

    project config changed

--- fmi-2024-01-spring-intro/src/main/java/course/spring/intro/web/HelloRestController.java
+++ fmi-2024-01-spring-intro/src/main/java/course/spring/intro/web/HelloRestController.java
@@ -1,15 +1,13 @@
 package course.spring.intro.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/hello")
 public class HelloRestController {
     @GetMapping(path = {"","/{name}"})
-    public String sayHello(@PathVariable(name = "name", required = false) String name){
-        return "Hello " + name + ", from my REST Endpoint!";
+    public String sayHello(@PathVariable(name = "name", required = false) String name,
+                           @RequestParam(value = "title", required = false) String title){
+        return "Hello "+ title + " " + name + ", from your REST Endpoint!";
     }
 }
