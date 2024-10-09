46ea0156674730de310459f6ed4b915217f1f036
Trayan Iliev <office@iproduct.org>
2024-10-09 20:47:02 +0300

    Post entity jpa annotations added

--- fmi-2024-01-spring-intro/src/main/java/course/spring/intro/entity/Post.java
+++ fmi-2024-01-spring-intro/src/main/java/course/spring/intro/entity/Post.java
@@ -1,22 +1,27 @@
 package course.spring.intro.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Collections;
 import java.util.Set;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NonNull
+    @EqualsAndHashCode.Include
     private Long id;
     @NonNull
     private String title;
     @NonNull
     private String content;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "TAGS")
     private Set<String> tags = Collections.EMPTY_SET;
 }
