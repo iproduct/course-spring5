132e1992d9c26d580ee9f4c3880761087cbabee6
Trayan Iliev <office@iproduct.org>
2024-10-16 21:03:58 +0300

    PostProvider impl added

---
+++ fmi-2024-02-core-xml/src/main/java/course/spring/domain/impl/SimplePostProvider.java
@@ -0,0 +1,20 @@
+package course.spring.domain.impl;
+
+import course.spring.domain.PostProvider;
+import course.spring.model.Post;
+
+import java.util.List;
+import java.util.Set;
+
+public class SimplePostProvider implements PostProvider {
+    @Override
+    public List<Post> getAllPosts() {
+        return List.of(
+        new Post(1L, "New in Spring 6", "API versions updated.", Set.of("spring 6", "novelty")),
+        new Post(2L, "Hibernate API Intrinsics", "Hibernate provides many important customization options.",
+                Set.of("hibernate", "jpa", "spring data jpa")),
+        new Post(3L, "Spring AI Integration", "Spring Boot provides OLLAMA starter.",
+                Set.of("spring", "spring boot", "ollama", "ai"))
+    );
+    }
+}
