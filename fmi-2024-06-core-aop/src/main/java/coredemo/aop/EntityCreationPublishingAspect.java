4c7459d1ee7fdf8129fa5db6ac568c07907b9e44
Trayan Iliev <office@iproduct.org>
2024-11-06 21:37:48 +0200

    all aspects enabled

--- fmi-2024-06-core-aop/src/main/java/coredemo/aop/EntityCreationPublishingAspect.java
+++ fmi-2024-06-core-aop/src/main/java/coredemo/aop/EntityCreationPublishingAspect.java
@@ -20,7 +20,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
-//@Aspect
+@Aspect
 public class EntityCreationPublishingAspect implements  ApplicationContextAware{
     final static Logger log = LoggerFactory.getLogger(EntityCreationPublishingAspect.class);
 
@@ -31,7 +31,7 @@ public class EntityCreationPublishingAspect implements  ApplicationContextAware{
     public void repositoryMethods() {
     }
 
-    @Pointcut("(execution(* *..add*(..)) || execution(* *..create*(..))) && args(article, ..)")
+    @Pointcut("(execution(* *..add*(coredemo.model.Article,..)) || execution(* *..create*(..))) && args(article, ..)")
     public void addMethods(Article article) {
     }
 
