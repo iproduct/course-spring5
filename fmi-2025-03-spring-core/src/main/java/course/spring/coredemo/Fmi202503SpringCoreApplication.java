a3bb6524050bd3f545ff99b726cbfae3ede10c19
Trayan Iliev <office@iproduct.org>
2025-10-24 21:01:02 +0300

    demo updated

---
+++ fmi-2025-03-spring-core/src/main/java/course/spring/coredemo/Fmi202503SpringCoreApplication.java
@@ -0,0 +1,13 @@
+package course.spring.coredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fmi202503SpringCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fmi202503SpringCoreApplication.class, args);
+    }
+
+}
---
+++ fmi-2025-03-spring-core/src/test/java/course/spring/fmi202503springcore/Fmi202503SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package course.spring.fmi202503springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fmi202503SpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
