24c2609d821c7cd4b7e07921084b9bfa0966cd7d
Trayan Iliev <office@iproduct.org>
2025-11-14 21:16:45 +0200

    ArticleController mapped to "/articles"

--- fmi-2025-05-webmvc-jpa/src/main/java/course/spring/intro/web/ArticleController.java
+++ fmi-2025-05-webmvc-jpa/src/main/java/course/spring/intro/web/ArticleController.java
@@ -1,7 +1,9 @@
 package course.spring.intro.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/articles")
 public class ArticleController {
 }
