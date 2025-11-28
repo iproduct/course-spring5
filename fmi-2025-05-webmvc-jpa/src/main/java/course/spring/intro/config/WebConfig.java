6c0d8115c65bf5f55418f74286248677620c3155
Trayan Iliev <office@iproduct.org>
2025-11-28 22:33:45 +0200

    I18n added

--- fmi-2025-05-webmvc-jpa/src/main/java/course/spring/intro/config/WebConfig.java
+++ fmi-2025-05-webmvc-jpa/src/main/java/course/spring/intro/config/WebConfig.java
@@ -1,10 +1,19 @@
 package course.spring.intro.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,5 +25,29 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadDir);
     }
+
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("bg", "BG"));
+        return localeResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
 
 }
--- fmi-2025-05-webmvc-jpa/src/main/resources/static/js/init.js
+++ fmi-2025-05-webmvc-jpa/src/main/resources/static/js/init.js
@@ -1,1 +1,10 @@
 M.AutoInit();
+$(document).ready(function() {
+    $(".locales").change(function () {
+        var selectedOption = $('.locales').val();
+        if (selectedOption != ''){
+            window.location.replace(window.location.pathname + '?lang=' + selectedOption);
+        }
+    });
+});
+
